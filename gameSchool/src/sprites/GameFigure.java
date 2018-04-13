0c4feea8a67fa6cc44ebddea29f356082cc7ab80
Alexander Strickner <alexstrickner00@gmail.com>
2018-04-13 15:32:15 +0200

    Adding config fxml

--- gameSchool/src/sprites/GameFigure.java
+++ gameSchool/src/sprites/GameFigure.java
@@ -1,5 +1,15 @@
 package sprites;
 
-public class GameFigure {
+import javafx.scene.image.Image;
 
+public class GameFigure extends Sprite {
+	private Image mainImage;
+	private Image[] walkImage;
+	private Image[] attackImage;
+	private int health;
+	private long lastAttack;
+
+	public GameFigure clone() {
+		return new GameFigure();
+	}
 }

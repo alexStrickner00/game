a10aba71af4bc1e35eca97f2974da889228a82c9
Alexander Strickner <alexstrickner00@gmail.com>
2018-04-20 09:00:27 +0200

    nothing special

--- gameSchool/src/game/Game.java
+++ gameSchool/src/game/Game.java
@@ -1,8 +1,29 @@
 package game;
 
+import java.util.ArrayList;
+
+import javafx.animation.AnimationTimer;
+import sprites.Sprite;
+
 public class Game {
 
+	private ArrayList<Sprite> ownSprites;
+	private ArrayList<Sprite>enemySprites;
+
+	public Game(){
+		
+	}
+	
 	public void run() {
+		
+		
+		AnimationTimer at = new AnimationTimer() {
+			
+			@Override
+			public void handle(long now) {
+				
+			}
+		};
 		
 	}
 

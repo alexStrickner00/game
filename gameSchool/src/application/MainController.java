328bd51495564b937534b580474af3be1f51cfcf
alex <alex@alex-stand-pc>
2018-04-24 21:01:14 +0200

    When starting new Game, an empty Pane is set as Scene + canvas

--- gameSchool/src/application/MainController.java
+++ gameSchool/src/application/MainController.java
@@ -3,9 +3,12 @@ package application;
 import game.Game;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.BorderPane;;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;;
 
 public class MainController {
 
@@ -14,6 +17,13 @@ public class MainController {
 	public void startGame(ActionEvent ae) {
 		
 		Node src = ((Node)ae.getSource());
+		
+		Stage stage = (Stage)src.getScene().getWindow();
+		
+		Pane gamePane = new Pane();
+		gamePane.setPrefSize(1000, 600);
+		stage.setScene(new Scene(gamePane));
+		
 		BorderPane bp = (BorderPane)src.getScene().getRoot();
 		
 		Canvas canvas = new Canvas(1000,600);

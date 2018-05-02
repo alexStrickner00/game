d5a3f3d6b9e21a4a6887b9025e6430baf543c8ad
alex <alex@alex-stand-pc>
2018-05-02 21:16:53 +0200

    added constants for images

--- gameSchool/src/sprites/GameFigure.java
+++ gameSchool/src/sprites/GameFigure.java
@@ -4,6 +4,11 @@ import javafx.scene.image.Image;
 
 public class GameFigure extends Sprite {
 
+	private static final int WALK_IMAGE1 = 0;
+	private static final int WALK_IMAGE2 = 1;
+	private static final int WALK_IMAGE3 = 2;
+	private static final int ATTACK_IMAGE = 3;
+	
 	protected int entityId;
 	protected Image[] walkImage;
 	protected Image[] attackImage;

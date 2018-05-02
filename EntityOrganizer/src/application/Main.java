5cd6a29f9afd7c80c9ad4342d9b734cd88e6e0ad
alex <alex@alex-stand-pc>
2018-05-02 22:30:05 +0200

    first start of design, no logic

--- EntityOrganizer/src/application/Main.java
+++ EntityOrganizer/src/application/Main.java
@@ -1,17 +1,17 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.TilePane;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			TilePane root = (TilePane)FXMLLoader.load(getClass().getResource("Main.fxml"));
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
